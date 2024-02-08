6574fbad536bd1e65d3b0157e0566f0028b306a0
Fernando Ramos Tutera <fernando.ramos@gmail.com>
2024-02-08 14:23:31 +0100

    Firstone

---
+++ README.md
@@ -0,0 +1,3 @@
+First clone the respository:
+
+https://github.com/New2Cde/Tp_1.git
---
+++ kanban-app/src/main/java/com/wkrzywiec/medium/kanban/service/KanbanServiceImpl.java
@@ -0,0 +1,87 @@
+package com.wkrzywiec.medium.kanban.service;
+
+import com.wkrzywiec.medium.kanban.model.Kanban;
+import com.wkrzywiec.medium.kanban.model.KanbanDTO;
+import com.wkrzywiec.medium.kanban.model.Task;
+import com.wkrzywiec.medium.kanban.model.TaskDTO;
+import com.wkrzywiec.medium.kanban.repository.KanbanRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class KanbanServiceImpl implements KanbanService {
+
+    private final KanbanRepository kanbanRepository;
+
+    @Override
+    @Transactional
+    public List<Kanban> getAllKanbanBoards() {
+        List<Kanban> kanbanList = new ArrayList<>();
+        kanbanRepository.findAll().forEach(kanbanList::add);
+        return kanbanList;
+    }
+
+    @SuppressWarnings("null")
+    @Override
+    @Transactional
+    public Optional<Kanban> getKanbanById(Long id) {
+        return kanbanRepository.findById(id);
+    }
+
+    @Override
+    @Transactional
+    public Optional<Kanban> getKanbanByTitle(String title) {
+        return kanbanRepository.findByTitle(title);
+    }
+
+    @SuppressWarnings("null")
+    @Override
+    @Transactional
+    public Kanban saveNewKanban(KanbanDTO kanbanDTO) {
+        return kanbanRepository.save(convertDTOToKanban(kanbanDTO));
+    }
+
+    @Override
+    @Transactional
+    public Kanban updateKanban(Kanban oldKanban, KanbanDTO newKanbanDTO) {
+        oldKanban.setTitle(newKanbanDTO.getTitle());
+        return kanbanRepository.save(oldKanban);
+    }
+
+    @SuppressWarnings("null")
+    @Override
+    @Transactional
+    public void deleteKanban(Kanban kanban) {
+        kanbanRepository.delete(kanban);
+    }
+
+    @Override
+    @Transactional
+    public Kanban addNewTaskToKanban(Long kanbanId, TaskDTO taskDTO) {
+        @SuppressWarnings("null")
+        Kanban kanban = kanbanRepository.findById(kanbanId).get();
+        kanban.addTask(convertDTOToTask(taskDTO));
+        return kanbanRepository.save(kanban);
+    }
+
+    private Kanban convertDTOToKanban(KanbanDTO kanbanDTO){
+        Kanban kanban = new Kanban();
+        kanban.setTitle(kanbanDTO.getTitle());
+        return kanban;
+    }
+
+    private Task convertDTOToTask(TaskDTO taskDTO) {
+        Task task = new Task();
+        task.setTitle(taskDTO.getTitle());
+        task.setDescription(taskDTO.getDescription());
+        task.setColor(taskDTO.getColor());
+        task.setStatus(taskDTO.getStatus());
+        return task;
+    }
+}
